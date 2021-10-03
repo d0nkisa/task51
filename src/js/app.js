92898625b0f3bde0f3e0fca97712b103619b348c
Donyo Donev <donevdonyo39@gmail.com>
2021-10-03 19:23:19 +0300

    hiding not active element 3s after loading the page

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,10 @@
 import "../scss/app.scss";
 
 window.addEventListener("DOMContentLoaded", () => {
-  // This block will be executed once the page is loaded and ready
+    // This block will be executed once the page is loaded and ready
+    setTimeout(() => {
+        var elementToHide = document.querySelectorAll('div.card:not(.active)');
+        elementToHide[0].style.display = 'none';
+        console.log(elementToHide);
+    }, 3000);
 });
